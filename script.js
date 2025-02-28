7744b8cb56b56126cc0571030be065d6c1a1a36f
Скляр Євген <145337848+MikotaGarci@users.noreply.github.com>
2025-02-28 09:32:54 +0000

    1

---
+++ script.js
@@ -0,0 +1,9 @@
+// Можна додати інтерактивні елементи, наприклад, плавний скролл
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
